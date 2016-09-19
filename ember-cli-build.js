913bee7056fa94be1722bd0a485d9c42816e1c1c
kwebster2 <kevin.r.webster@gmail.com>
2016-09-19 17:15:28 -0500

    added css and js vendor dependences

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+        // Add options here
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -19,6 +19,26 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+  app.import('vendor/assets/css/bootstrap.min.css');
+  app.import('vendor/assets/css/animate.css');
+  app.import('vendor/assets/css/font-awesome.min.css');
+  app.import('vendor/assets/css/magnific-popup.css');
+  app.import('vendor/assets/css/owl.carousel.css');
+  app.import('vendor/assets/css/owl.theme.css');
+  app.import('vendor/assets/css/simpletextrotator.css');
+  app.import('vendor/assets/css/style.css');
+  app.import('vendor/assets/js/jquery-1.11.0.min.js');
+  app.import('vendor/assets/js/jquery.backstretch.min.js');
+  app.import('vendor/assets/js/jquery.countTo.js');
+  app.import('vendor/assets/js/jquery.fitvids.js');
+  app.import('vendor/assets/js/jquery.magnific-popup.min.js');
+  app.import('vendor/assets/js/jquery.simple-text-rotator.min.js');
+  app.import('vendor/assets/js/jquery.waypoints.js');
+  app.import('vendor/assets/js/bootstrap.min.js');
+  app.import('vendor/assets/js/owl.carousel.min.js');
+  app.import('vendor/assets/js/smoothscroll.js');
+  app.import('vendor/assets/js/wow.min.js');
+  app.import('vendor/assets/js/custom.js');
 
   return app.toTree();
 };
